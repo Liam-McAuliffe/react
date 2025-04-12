9d4e11e0211c624cca5542eaaa9ed3377dcd7ac2
Liam-McAuliffe <lmmcauliffe1223@gmail.com>
2025-04-12 11:46:42 -0400

    init

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,10 @@
+import { Button } from '../ui/button';
+
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Component</h1>
+      <Button variant="link">Click me</Button>
     </div>
   );
 };
